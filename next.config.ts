d6eb759480470a9383ea18061bc9924071fcf1ac
kaviska <126580314+kaviska@users.noreply.github.com>
2025-08-14 09:16:18 +0530

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  // ❌ Disable ESLint checking
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
---
+++ src/components/CreativeEditorSDK.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
+import CreativeEditorSDK from '@cesdk/cesdk-js';
+
+// configure CreativeEditor SDK
+const config = {
+  license: '3z8cnvFRPB3b4wWvHCObxc5TWhOxExkzTgyomTj6V1kjTrCq0Gd9AgfuzJQl9CdR', // replace with a valid CE.SDK license key
+  callbacks: { onUpload: 'local' as const }, // enable local file uploads in the Asset Library
+};
+
+export default function CreativeEditorSDKComponent() {
+  // reference to the container HTML element where CESDK will be initialized
+  const cesdk_container = useRef(null);
+  // define a state variable to keep track of the CESDK instance
+  const [cesdk, setCesdk] = useState<CreativeEditorSDK | null>(null);
+
+  useEffect(() => {
+    // prevent initialization if the container element is not available yet
+    if (!cesdk_container.current) {
+      return;
+    }
+
+    // flag to keep track of the component unmount
+    let cleanedUp = false;
+    // where to store the local CE.SDK instance
+    //@ts-ignore
+    let instance;
+
+    // initialize the CreativeEditorSDK instance in the container HTML element
+    // using the given config
+    CreativeEditorSDK.create(cesdk_container.current, config).then(
+      async _instance => {
+        // assign the current CD.SDK instance to the local variable
+        instance = _instance;
+
+        if (cleanedUp) {
+          instance.dispose();
+          return;
+        }
+
+        // do something with the instance of CreativeEditor SDK (e.g., populate
+        // the asset library with default / demo asset sources)
+        await Promise.all([
+          instance.addDefaultAssetSources(),
+          instance.addDemoAssetSources({ sceneMode: 'Video' }),
+        ]);
+
+        // create a new design scene in the editor
+        await instance.createVideoScene();
+
+        // store the CESDK instance in state
+        setCesdk(instance);
+      },
+    );
+
+    // cleanup function to dispose of the CESDK instance when the component unmounts
+    const cleanup = () => {
+      // clear the local state and dispose of the CS.SDK instance (if it has been assigned)
+      cleanedUp = true;
+      //@ts-ignore
+      instance?.dispose();
+      setCesdk(null);
+    };
+    
+
+    // to ensure cleanup runs when the component unmounts
+    return cleanup;
+
+    
+  }, [cesdk_container]);
+
+  
+
+  return (
+    // the container HTML element where the CESDK editor will be mounted
+    <div
+      ref={cesdk_container}
+      style={{ width: '100vw', height: '100vh' }}
+    ></div>
+  );
+}
